7ef5cb98930aceebdea1726dd180efcf0cb1eb95
Chevvuri Karthik <87626375+Ftdgufvfi@users.noreply.github.com>
2023-12-30 22:29:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-r1 = Math.floor(Math.random())*6+1;
+r1 = Math.floor(Math.random()*6)+1;
 var a = document.querySelector(".img1");
-a.setAttribute("src","./images/dice"+r1+".png");
+a.setAttribute("src", "./dice"+r1+".png");
+r2 = Math.floor(Math.random()*6)+1;
+var b = document.querySelector(".img2");
+b.setAttribute("src", "./dice"+r2+".png");
+if(r1>r2)
+{
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if(r1<r2)
+{
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "🚩 Draw! 🚩";
+}
